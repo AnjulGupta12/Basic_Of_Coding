1f6c129544897569b62795bf71fb739eedc24a1e
Anjul Gupta <76518288+AnjulGupta12@users.noreply.github.com>
2022-08-12 15:47:52 +0530

    Create 6.java

---
+++ 6.java
@@ -0,0 +1,10 @@
+class Days {
+    public static void main(String[] args) {
+        public void Days(int days)
+        {
+          double month = days/30;
+          double day = days%30;
+          System.out.print("Number of month "+month+" and days left are "+day);
+        }
+    }
+}
