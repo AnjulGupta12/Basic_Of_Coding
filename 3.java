278d1788b23ec5f1013bdb188a60f962d61901ce
Anjul Gupta <76518288+AnjulGupta12@users.noreply.github.com>
2022-08-12 15:23:47 +0530

    Create 3.java

---
+++ 3.java
@@ -0,0 +1,10 @@
+
+class RateOfInterest {
+    public static void main(String[] args) {
+        public void rateOfInterest(int P, int R, int T)
+        {
+            int SI = (P*R*T)/100;
+            return SI;
+        }
+    }
+}
