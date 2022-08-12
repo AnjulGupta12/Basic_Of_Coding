284f2bfe243dc0607427769ed332b498518363cd
Anjul Gupta <76518288+AnjulGupta12@users.noreply.github.com>
2022-08-12 20:23:05 +0530

    Create 13.java

---
+++ 13.java
@@ -0,0 +1,29 @@
+class greaterFromThreeNo {
+    public static void main(String[] args) {
+        public void greatestFromThreeNo(double a,double b, double c)
+        {
+         if(a>b)
+         {
+            if(a>c)
+            {
+            System.out.print(a+" is greater.");
+            }
+            else
+            {
+            System.out.print(b+" is greater.");
+            }
+         }
+          else
+          {
+            if(b>c){
+            System.out.print(b+" is greater.");
+            }
+            else
+            {
+            System.out.print(c+" is greater.");
+            }
+          
+          }
+        }
+    }
+}
