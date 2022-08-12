b71e18a238e3cb9912a9249c8121536feaf49eff
Anjul Gupta <76518288+AnjulGupta12@users.noreply.github.com>
2022-08-12 15:27:50 +0530

    Create 4.java

---
+++ 4.java
@@ -0,0 +1,9 @@
+class FarenhiteToDegree {
+    public static void main(String[] args) {
+        public double farenhiteToDegree(double F)
+        {
+            double C = 5*(f-32)/9;
+            return C;
+        }
+    }
+}
