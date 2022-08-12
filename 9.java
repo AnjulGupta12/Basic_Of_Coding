57f0690bfd519bdf6bc459c4168891932345435c
Anjul Gupta <76518288+AnjulGupta12@users.noreply.github.com>
2022-08-12 19:16:36 +0530

    Create 9.java

---
+++ 9.java
@@ -0,0 +1,14 @@
+class checkNegativeAndPositive {
+    public static void main(String[] args) {
+        public void checkNegativeAndPositive(double number)
+        {
+            if(number>0){
+            System.out.print(number + " is positive.");
+            }
+          else
+            ){
+            System.out.print(number + " is negative.");
+            }
+        }
+    }
+}
