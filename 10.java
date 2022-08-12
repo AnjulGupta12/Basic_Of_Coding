a955307798e09e179a823caaa82489c90b91e480
Anjul Gupta <76518288+AnjulGupta12@users.noreply.github.com>
2022-08-12 19:35:23 +0530

    Create 10.java

---
+++ 10.java
@@ -0,0 +1,14 @@
+ class divisibilityWith47 {
+    public static void main(String[] args) {
+        public void divisibilityWith47(double number)
+        {
+            if(number%47==0){
+            System.out.print(number + " is divisible by 47.");
+            }
+          else
+            {
+            System.out.print(number + " is not divisible by 47.");
+            }
+        }
+    }
+}
