23a54f48289c197f268c73f3fdc50cd4226d85d2
Anjul Gupta <76518288+AnjulGupta12@users.noreply.github.com>
2022-08-12 15:41:45 +0530

    Create 5.java

---
+++ 5.java
@@ -0,0 +1,11 @@
+class RemainderAndQuotient {
+    public static void main(String[] args) {
+        public void reminderAndQuotient(int Numeratre, int Denominator)
+        {
+            double Remainder = Numeratre % Denominator;
+            double Quotient = Numerator / Denominator;
+            System.out.print(Remainder);
+            System.out.print(Quotient);
+        }
+    }
+}
