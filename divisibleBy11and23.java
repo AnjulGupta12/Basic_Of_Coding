91de669218d5ab1db9512b3b3c72789e2d82c4e6
Anjul Gupta <76518288+AnjulGupta12@users.noreply.github.com>
2022-08-12 19:46:55 +0530

    Create divisibleBy11and23.java

---
+++ divisibleBy11and23.java
@@ -0,0 +1,22 @@
+class checkBetween11and23 {
+    public static void main(String[] args) {
+        public void checkBetween11and23(double number)
+        {
+            if(number>11)
+            {
+              if(number<23)
+              {
+                System.out.print(number + " is in between 11 and 23.");
+              }
+              else
+              {
+                System.out.print(number + " is not in between 11 and 23.");
+              } 
+            }
+          else
+             {
+                System.out.print(number + " is not in between 11 and 23.");
+             }
+        }
+    }
+}
