27e3a11bb1a3534e384a695c3e8123f59f5acd2f
Anjul Gupta <76518288+AnjulGupta12@users.noreply.github.com>
2022-08-12 16:16:10 +0530

    Create 8.java

---
+++ 8.java
@@ -0,0 +1,15 @@
+class GreatestOfTwo {
+    public static void main(String[] args) {
+        public void greatestAmongTwo(int a1, int a2)
+        {
+            if(a1>a2)
+            {
+              System.out.print(a1 +" is greatest");
+            }
+          else
+          {
+            System.out.print(a2 +" is greatest");}
+          }    
+      }
+    }
+}
