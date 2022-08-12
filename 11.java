938ccd979f4264e65070c60134984337a4f78c29
Anjul Gupta <76518288+AnjulGupta12@users.noreply.github.com>
2022-08-12 19:41:03 +0530

    Create 11.java

---
+++ 11.java
@@ -0,0 +1,14 @@
+class evenAndOdd {
+    public static void main(String[] args) {
+        public void evenAndOdd(double number)
+        {
+            if(number%2==0){
+            System.out.print(number + " is even.");
+            }
+          else
+            {
+            System.out.print(number + " is odd.");
+            }
+        }
+    }
+}
