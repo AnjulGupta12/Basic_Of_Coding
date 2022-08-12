afffd4f05ddc0eb5201835e42e841dd05ed5a3d8
Anjul Gupta <76518288+AnjulGupta12@users.noreply.github.com>
2022-08-12 15:52:17 +0530

    Create 7.java

---
+++ 7.java
@@ -0,0 +1,9 @@
+class AreaCircle {
+    public static void main(String[] args) {
+        public void areaOfCircle(double radius)
+        {
+            double area = 3.14*radius*radius;
+            System.out.print(area);
+        }
+    }
+}
