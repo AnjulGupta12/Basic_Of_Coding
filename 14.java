a98ab41c1c4dcdf0a78a871f07c9924b0296b7d4
Anjul Gupta <76518288+AnjulGupta12@users.noreply.github.com>
2022-08-12 21:09:13 +0530

    Create 14.java

---
+++ 14.java
@@ -0,0 +1,46 @@
+class RupeeToPaise {
+    public static void main(String[] args) {
+      public void RupeeToPaise(int a, int b, int c, int d)
+      {
+      
+      }
+      
+
+      
+      
+      
+      
+System.out.println(paise);
+
+    }
+}
+/*
+if(a>b)
+{
+    if(a>c)
+    {
+       if(a>d)    [a]
+       else       [c]
+    }
+    else
+    {
+       if(c>d)    [c]
+       else       [d]       
+    }
+}
+else
+{
+  if(b>c)
+  {
+      if(b>d)     [b]
+      else        [c]
+  }
+  else
+  {
+      if(c>d)     [c]
+      else        [d]
+  }
+}
+
+
+*/
