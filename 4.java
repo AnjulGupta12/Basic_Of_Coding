dca0d36e2c04028d39fc12ac8b775c5a93696ead
Anjul Gupta <76518288+AnjulGupta12@users.noreply.github.com>
2022-08-12 15:29:11 +0530

    Update 4.java

--- 4.java
+++ 4.java
@@ -1,9 +1,9 @@
 class FarenhiteToDegree {
     public static void main(String[] args) {
-        public double farenhiteToDegree(double F)
+        public void farenhiteToDegree(double F)
         {
             double C = 5*(f-32)/9;
-            return C;
+            System.out.print(C);
         }
     }
 }
